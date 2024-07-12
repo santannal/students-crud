83d0761a129706b12b1c37695cbe1e90ce242f33
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-07-12 09:44:25 -0300

    closes #2

--- students-backend/src/main/java/com/students/models/Student.java
+++ students-backend/src/main/java/com/students/models/Student.java
@@ -1,5 +1,74 @@
 package com.students.models;
 
 public class Student {
-    
+    //Attributes
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private int idCurso;
+    private int period;
+
+    //builders
+    public Student(int id, String name, String email, String phone, int idCurso, int period) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.idCurso = idCurso;
+        this.period = period;
+    }
+
+    public Student(){
+
+    }
+
+    //Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
 }
