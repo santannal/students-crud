34727be837493276eed3cd86c74bc64ef69d9579
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-07-12 10:57:44 -0300

    closes #6

--- students-backend/src/main/java/com/students/resources/StudentController.java
+++ students-backend/src/main/java/com/students/resources/StudentController.java
@@ -1,5 +1,6 @@
 package com.students.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,20 +9,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.students.models.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @CrossOrigin
 public class StudentController {
     private List<Student> students = new ArrayList<>();
 
-    /*
-     * new Student(1, "student 1", "Email1", "11973631403", 1, 1),
-     * new Student(2, "student 2", "Email2", "112222222222", 2, 2));
-     */
+    @PostMapping("students")
+    public ResponseEntity<Student> save(@RequestBody Student student) {
+        student.setId(students.size() + 1);
+        students.add(student);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(student.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(student);
+    }
 
     @GetMapping("students/{id}")
     public ResponseEntity<Student> getStudent(@PathVariable int id) {
